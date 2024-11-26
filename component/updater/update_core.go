2355d88d377723c725e11d73d507cd11ec65a01e
lux5am <lux.5am@yandex.com>
2024-11-27 02:46:11 +0800

    chore: change update url
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- component/updater/update_core.go
+++ component/updater/update_core.go
@@ -24,11 +24,11 @@ import (
 )
 
 const (
-	baseReleaseURL    = "https://github.com/MetaCubeX/mihomo/releases/latest/download/"
-	versionReleaseURL = "https://github.com/MetaCubeX/mihomo/releases/latest/download/version.txt"
+	baseReleaseURL    = "https://github.com/lux5am/mihomo/releases/latest/download/"
+	versionReleaseURL = "https://github.com/lux5am/mihomo/releases/latest/download/version.txt"
 
-	baseAlphaURL    = "https://github.com/vernesong/mihomo/releases/download/Prerelease-Alpha/"
-	versionAlphaURL = "https://github.com/vernesong/mihomo/releases/download/Prerelease-Alpha/version.txt"
+	baseAlphaURL    = "https://github.com/lux5am/mihomo-smart/releases/download/Prerelease-Alpha/"
+	versionAlphaURL = "https://github.com/lux5am/mihomo-smart/releases/download/Prerelease-Alpha/version.txt"
 
 	// MaxPackageFileSize is a maximum package file length in bytes. The largest
 	// package whose size is limited by this constant currently has the size of
